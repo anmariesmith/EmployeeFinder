a3f04916a8fc3139b872a3d9ea09bc4bde7cda79
Anmarie Smith <anmariesmith007@gmail.com>
2018-10-31 10:23:36 -0400

    fixed looping issue in apiroutes

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,45 +1,55 @@
-let employeeList = require("../data/employees.js");
-let count = 0;
+const employee = require("../data/employees");
 
 module.exports = function(app) {
   /**
    * GET the employeeList
    */
   app.get("/api/employees", function(req, res) {
-    res.json(employeeList);
+    res.json(employee);
   });
+
   app.post("/api/employees", function(req, res) {
     let bestMatch = {
       name: "",
       photo: "",
-      employeeDifference: 1000
+      employeeDifference: Infinity
     };
+
     console.log(req.body);
+
     const userData = req.body;
     const userScores = userData.scores;
 
-    for (let i = 0; i < employeeList.length; i++) {
-      let totalDifference = 0;
-      let currentEmployee = employeeList[i];
+    let totalDifference;
+
+    for (let i = 0; i < employee.length; i++) {
+      let currentEmployee = employee[i];
+      totalDifference = 0;
 
       for (let j = 0; j < currentEmployee.scores.length; j++) {
         const currentEmployeeScore = currentEmployee.scores[j];
         const currentUserScore = userScores[j];
+
         console.log(currentUserScore);
         console.log(`----EMPLOYE NAME: ${currentEmployee.name} SCORES----`);
         console.log(currentEmployeeScore);
         console.log(`----EMPLOYE NAME: USER INPUT SCORES----`);
 
-        totalDifference += Math.abs(currentEmployeeScore - currentUserScore);
+        totalDifference += Math.abs(
+          parseInt(currentEmployeeScore) - parseInt(currentUserScore)
+        );
       }
       console.log(`TOTAL DIFFERENCE: ${totalDifference}`);
-      if (totalDifference < bestMatch.employeeDifference) {
+
+      if (totalDifference <= bestMatch.employeeDifference) {
         bestMatch.name = currentEmployee.name;
         bestMatch.photo = currentEmployee.photo;
         bestMatch.employeeDifference = totalDifference;
       }
-      res.json(bestMatch);
     }
-    res.end();
+
+    employee.push(userData);
+
+    res.json(bestMatch);
   });
 };
